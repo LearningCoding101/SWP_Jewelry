7b83be225948a17767d5320313652298400c8d58
LearningCoding101 <hahoang33322@gmail.com>
2024-05-25 12:01:25 +0700

    implemented password encryption

--- src/main/java/com/shop/JewleryMS/Entity/Account.java
+++ src/main/java/com/shop/JewleryMS/Entity/Account.java
@@ -3,12 +3,16 @@ package com.shop.JewleryMS.Entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "Account")
 @Getter
 @Setter
-public class Account {
+public class Account implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "PK_userID")
@@ -21,4 +25,39 @@ public class Account {
     String aPassword;
     @Column(name = "accountName")
     String accountName;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.aPassword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.aUsername;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
