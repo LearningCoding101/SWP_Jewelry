f3157e56d0a7cbb9a4ad83d8035bbdfe312713c1
Lâm Tấn Phú <truongphi246357@gmail.com>
2024-05-29 10:34:24 +0700

    CustomerCRUD

--- src/main/java/com/shop/JewleryMS/repository/PromotionRepository.java
+++ src/main/java/com/shop/JewleryMS/repository/PromotionRepository.java
@@ -2,7 +2,9 @@ package com.shop.JewleryMS.repository;
 
 import com.shop.JewleryMS.entity.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
 
 }
