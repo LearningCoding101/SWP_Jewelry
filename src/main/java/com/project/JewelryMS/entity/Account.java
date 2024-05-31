0dddfe799f148d016106b8cd66d0b8fac921ad49
Dang <thaidangnguyen2004@gmail.com>
2024-05-31 15:32:07 +0700

    ProductSell

--- src/main/java/com/project/JewelryMS/entity/Account.java
+++ src/main/java/com/project/JewelryMS/entity/Account.java
@@ -1,5 +1,6 @@
 package com.project.JewelryMS.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -38,6 +39,7 @@ public class Account implements UserDetails {
     Boolean status;
 
     @OneToOne(mappedBy = "account", cascade = CascadeType.ALL)
+    @JsonManagedReference
     private StaffAccount staffAccount;
 
     @Override
