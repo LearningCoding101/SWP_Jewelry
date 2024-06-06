4124ab7bd1f7a6b3bc9bfc78a1f98258f03b92b6
LearningCoding101 <hahoang33322@gmail.com>
2024-06-06 14:45:47 +0700

    fixed null category exception

--- src/main/java/com/project/JewelryMS/model/Order/CreateOrderWrapper.java
+++ src/main/java/com/project/JewelryMS/model/Order/CreateOrderWrapper.java
@@ -1,10 +1,12 @@
 package com.project.JewelryMS.model.Order;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 
 @Data
+@ToString
 public class CreateOrderWrapper {
     private CreateOrderRequest orderRequest;
     private List<CreateOrderDetailRequest> detailList;
