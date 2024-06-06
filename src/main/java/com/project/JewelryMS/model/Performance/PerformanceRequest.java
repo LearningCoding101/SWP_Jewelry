3a8aebefca2a628c3176370f2d14d8b44569e483
Lâm Tấn Phú <truongphi246357@gmail.com>
2024-06-06 14:52:46 +0700

    Performance + fix issues

---
+++ src/main/java/com/project/JewelryMS/model/Performance/PerformanceRequest.java
@@ -0,0 +1,21 @@
+package com.project.JewelryMS.model.Performance;
+
+import com.project.JewelryMS.entity.StaffAccount;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class PerformanceRequest {
+    long PK_performanceID;
+
+    private Date date;
+    private int salesMade;
+    private double revenueGenerated;
+    private int customerSignUp;
+
+    //no need to request staff id in the update
+//    private int staffID;
+
+
+}
