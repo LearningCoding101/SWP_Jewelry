9d555baec0a5c02ed35914a530b83c4c0c0114f6
Dang <thaidangnguyen2004@gmail.com>
2024-06-12 17:36:59 +0700

    DANGHNT/ProductBuyAPI

--- src/main/java/com/project/JewelryMS/model/ProductBuy/CreateProductBuyResponse.java
+++ src/main/java/com/project/JewelryMS/model/ProductBuy/CreateProductBuyResponse.java
@@ -3,17 +3,16 @@ package com.project.JewelryMS.model.ProductBuy;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateProductBuyRequest {
-    private String pbName;
+public class CreateProductBuyResponse {
+    private Long ProductBuyID;
     private String categoryName;
+    private String pbName;
     private String metalType;
     private String gemstoneType;
-    private MultipartFile image;
-    private Integer chi;
-    private Integer carat;
+    private String image;
+    private Float cost;
 }
--- src/main/java/com/project/JewelryMS/model/ProductBuy/ProductBuyResponse.java
+++ src/main/java/com/project/JewelryMS/model/ProductBuy/ProductBuyResponse.java
@@ -3,25 +3,16 @@ package com.project.JewelryMS.model.ProductBuy;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductBuyResponse {
-    private Long buyID;
-    private Long productSellID;
-    private Long orderID;
-    private Long categoryID;
-    private Float appraisalValue;
-    private String pcondition;
-    private Float weight;
-    private String description;
+    private Long ProductBuyID;
+    private String categoryName;
+    private String pbName;
     private String metalType;
     private String gemstoneType;
-    private String productCode;
-    private String manufacturer;
-    private Integer amount;
-    private byte[] image;
-    private Integer chi;
-    private Integer carat;
+    private Float cost;
 }
