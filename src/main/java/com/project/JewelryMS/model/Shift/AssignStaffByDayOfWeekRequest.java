f4fde0cd28941364b7a17428d6f48fc5a4cf1734
LearningCoding101 <hahoang33322@gmail.com>
2024-06-27 11:07:15 +0700

    temp

--- src/main/java/com/project/JewelryMS/model/Shift/AssignStaffByDayOfWeekRequest.java
+++ src/main/java/com/project/JewelryMS/model/Shift/AssignStaffByDayOfWeekRequest.java
@@ -1,10 +1,22 @@
 package com.project.JewelryMS.model.Shift;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 import java.time.DayOfWeek;
@@ -15,9 +27,12 @@ import java.util.Map;
 @NoArgsConstructor
 @Data
 public class AssignStaffByDayOfWeekRequest {
+    @JsonProperty("staffAvailabilityByDayOfWeek")
+
     private Map<Integer, Map<DayOfWeek, List<String>>> staffAvailability;
+
     private LocalDate startDate;
     private LocalDate endDate;
-
 }
+
 
