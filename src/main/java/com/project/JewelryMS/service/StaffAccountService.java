cd8d6c7cc9086ed08b5150b4f4678d4822077ce9
Lâm Tấn Phú <truongphi246357@gmail.com>
2024-07-07 14:00:44 +0700

    Small changes to schedule

--- src/main/java/com/project/JewelryMS/service/StaffAccountService.java
+++ src/main/java/com/project/JewelryMS/service/StaffAccountService.java
@@ -110,6 +110,7 @@ public class StaffAccountService {
         account.setAUsername(staffAccountRequest.getUsername());
 //        account.setAPassword(passwordEncoder.encode(staffAccountRequest.getPassword()));
         account.setAccountName(staffAccountRequest.getAccountName());
+        account.setRole(staffAccountRequest.getRole()); // Update role from request
 
         // Save the updated entities
         authenticationRepository.save(account);
