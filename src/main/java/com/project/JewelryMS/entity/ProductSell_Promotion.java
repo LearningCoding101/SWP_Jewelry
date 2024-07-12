958a706b920092f7e57f4977e4f97ebd18c8401c
LearningCoding101 <hahoang33322@gmail.com>
2024-07-13 00:20:38 +0700

    attempt change

--- src/main/java/com/project/JewelryMS/entity/ProductSell_Promotion.java
+++ src/main/java/com/project/JewelryMS/entity/ProductSell_Promotion.java
@@ -1,15 +1,19 @@
 package com.project.JewelryMS.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(exclude = {"productSell", "promotion"})
+
 public class ProductSell_Promotion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,10 +23,14 @@ public class ProductSell_Promotion {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "productsellID", referencedColumnName = "PK_productID")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JsonBackReference
+
     private ProductSell productSell;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "promotionID", referencedColumnName = "PK_promotionID")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JsonBackReference
+
     private Promotion promotion;
 }
