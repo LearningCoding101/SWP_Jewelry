5015f1db6bd4c80e1e7d1d6bc06884e24e78bfc5
Dang <thaidangnguyen2004@gmail.com>
2024-07-13 18:37:33 +0700

    DANGHNT/Validate-Staff-ProductBuy-ProductPricing

--- src/main/java/com/project/JewelryMS/controller/ProductSellController.java
+++ src/main/java/com/project/JewelryMS/controller/ProductSellController.java
@@ -7,13 +7,9 @@ import com.project.JewelryMS.model.Promotion.AssignPromotionRequest;
 import com.project.JewelryMS.service.ProductSellService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartException;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -27,7 +23,7 @@ public class ProductSellController {
 
     // Create a new ProductSell
     @PostMapping
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<ProductSellResponse> createProductSell(@ModelAttribute CreateProductSellRequest createProductSellRequest) {
         ProductSellResponse createdProduct = productSellService.createProductSell(createProductSellRequest);
         return ResponseEntity.ok(createdProduct);
@@ -49,7 +45,6 @@ public class ProductSellController {
 
     // Get a ProductSell by ID
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_STAFF')")
     public ResponseEntity<ProductSellResponse> getProductSellById(@PathVariable long id) {
         ProductSellResponse response = productSellService.getProductSellById2(id);
         return ResponseEntity.ok(response);
@@ -57,7 +52,7 @@ public class ProductSellController {
 
     // Update a ProductSell by ID
     @PutMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<ProductSellResponse> updateProductSell(@PathVariable long id, @ModelAttribute ProductSellRequest productSellRequest) {
         ProductSellResponse updatedProduct = productSellService.updateProductSell(id, productSellRequest);
         return ResponseEntity.ok(updatedProduct);
@@ -65,7 +60,7 @@ public class ProductSellController {
 
     // Delete a ProductSell by ID
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<String> deleteProductSell(@PathVariable long id) {
         productSellService.deleteProduct(id);
         return ResponseEntity.ok("Product deleted successfully");
@@ -73,7 +68,7 @@ public class ProductSellController {
 
     // Adjust pricing ratio
     @PostMapping("/adjust-ratio/{ratio}")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<Float> adjustRatio(@PathVariable Float ratio) {
         productSellService.updatePricingRatioPS(ratio);
         return ResponseEntity.ok(ratio);
@@ -81,7 +76,7 @@ public class ProductSellController {
 
     // Add promotions to a ProductSell
     @PostMapping("/promotions")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<String> addPromotionsToProductSell(@RequestBody AddPromotionsRequest request) {
         ProductSell updatedProductSell = productSellService.addPromotionsToProductSell(request);
         return ResponseEntity.ok("Add Successfully");
@@ -89,28 +84,28 @@ public class ProductSellController {
 
     // Remove promotions from a ProductSell
     @DeleteMapping("/promotions")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<String> removePromotionsFromProductSell(@RequestBody RemovePromotionRequest request) {
         ProductSell updatedProductSell = productSellService.removePromotionsFromProductSell(request);
         return ResponseEntity.ok("Remove Successfully");
     }
 
     @PostMapping("/assign-promotion-to-various-products")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<String> assignPromotionToProductSells(@RequestBody AssignPromotionRequest request) {
         productSellService.assignPromotionToProductSells(request);
         return ResponseEntity.ok("Promotion assigned to product sells successfully");
     }
 
     @DeleteMapping("/remove")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<String> removePromotionFromProductSells(@RequestBody AssignPromotionRequest request) {
         productSellService.removePromotionFromProductSells(request);
         return ResponseEntity.ok("Promotion removed from product sells successfully");
     }
 
     @DeleteMapping("/remove-all-promotion-from-product")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER')")
     public ResponseEntity<String> removeAllPromotionsFromProductSells(@RequestBody List<Long> productSellIds) {
         productSellService.removeAllPromotionsFromProductSells(productSellIds);
         return ResponseEntity.ok("All promotions removed from product sells successfully");
@@ -118,7 +113,7 @@ public class ProductSellController {
 
     // Get product by guarantee ID
     @GetMapping("/by-guarantee-id")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_STAFF')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_STAFF')")
     //@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_STAFF', 'ROLE_MANAGER')")
     public ResponseEntity<ProductSellResponse> readGuaranteeByGuaranteeId(@RequestParam Long guaranteeId) {
         ProductSellResponse productSellResponse = productSellService.readProductByGuaranteeId(guaranteeId);
@@ -126,7 +121,7 @@ public class ProductSellController {
     }
 
     @GetMapping("/get-by-code")
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_STAFF')")
+//    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_MANAGER', 'ROLE_STAFF')")
     public ResponseEntity<ProductSellResponse> getProductByCode(@RequestParam String productCode) {
         ProductSellResponse productSellResponse = productSellService.findByProductCode(productCode);
         return ResponseEntity.ok(productSellResponse);
