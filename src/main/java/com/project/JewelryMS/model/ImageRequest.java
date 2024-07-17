d545792a7d66c7ab0fa8eec2f99a76085dd63ec7
Dang <thaidangnguyen2004@gmail.com>
2024-07-17 15:09:14 +0700

    DANGHNT/Image-Controller-Update

---
+++ src/main/java/com/project/JewelryMS/model/ImageRequest.java
@@ -0,0 +1,12 @@
+package com.project.JewelryMS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageRequest {
+    String image;
+}
